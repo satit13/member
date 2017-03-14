712ae75b2efa6a9820c743cf26b05e4c0d3a0705
= <=>
2017-03-14 16:19:01 +0700

    first source code - member mock to calculate point

---
+++ model/sale.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"time"
+//	"fmt"
+)
+
+type  Sale struct{
+
+	Docno string
+	Docdate time.Time
+	Totalamount float64
+	Point int
+	Arcode string
+}
+type Sales []*Sale
+
+func (s *Sale)GetbyMember()(sl Sales){
+
+	db,err := ConnectMssql()
+	rows , err := db.Query("select docno,docdate,totalamount from pos.dbo.bcarinvoice where arcode = ?",s.Arcode)
+
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+	var cts Sales
+
+	for rows.Next() {
+		var s = new(Sale)
+		rows.Scan(
+			&s.Docno,
+			&s.Docdate,
+			&s.Totalamount,
+		)
+
+		cts =  append(cts, s)
+
+
+	}
+	return cts
+
+
+
+
+}
+
+
+
