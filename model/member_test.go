301b3d08dcdfa9e8306261ff2677ffbf3442f3c2
= <=>
2017-03-14 17:08:51 +0700

    Get sale by arcode

--- model/member_test.go
+++ model/member_test.go
@@ -9,108 +9,95 @@ import (
 	//"github.com/revel/modules/db/app"
 )
 
-func TestConnectMsSQL(t *testing.T) {
-	_,err := model.ConnectMssql()
-	if err != nil{
-		fmt.Println("MsSql Connect Error : ",err)
-		return
-	}
-	fmt.Println("connected")
 
-	//err = db.Ping()
-	//if err != nil{
-	//	fmt.Println("ERROR!!!!! -> ",err)
-	//}
+func TestMember_Create(t *testing.T){
+	//arrange
+	m1 := &model.Member{Name: "satit", Code: "0816398388"}
+	fmt.Println(m1)
+
+	//act
+	m1.Reset()
+
+	id,err := m1.Create()
+
+	fmt.Println("new id is : " , id)
+	fmt.Println(m1)
+	if err != nil {
+		t.Error(err)
+	}
 }
 
-//func TestMember_Create(t *testing.T){
-//	//arrange
-//	m1 := &model.Member{Name: "satit", Code: "0816398388"}
-//	fmt.Println(m1)
-//
-//	//act
-//	m1.Reset()
-//
-//	id,err := m1.Create()
-//
-//	fmt.Println("new id is : " , id)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//}
-
-//func TestMember_Update(t *testing.T){
-//	m1 := &model.Member{Id:1,Name: "satitx", Code: "0816398388"}
-//	fmt.Println(`update to `,m1)
-//
-//
-//	//act
-//	//m1.Reset()
-//
-//	row,err := m1.Update()
-//
-//	fmt.Println("row effected is  : " , row)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//}
+func TestMember_Update(t *testing.T){
+	m1 := &model.Member{Id:1,Name: "satitx", Code: "0816398388"}
+	fmt.Println(`update to `,m1)
 
 
-//func TestMember_DuplicateName(t *testing.T){
-//	m1 := &model.Member{Name: "satit", Code: "0816398388"}
-//	fmt.Println(m1)
-//
-//	//act
-//	m1.Reset()
-//
-//
-//	//fist insert code 0816398388
-//	id,err := m1.Create()
-//	fmt.Println("new id is : " , id)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	//fist insert code 0816398388
-//	m1.Code = "999"
-//	id,err = m1.Create()
-//	fmt.Println("new id is : " , id)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//
-//}
+	//act
+	//m1.Reset()
+
+	row,err := m1.Update()
+
+	fmt.Println("row effected is  : " , row)
+	fmt.Println(m1)
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 
-//func TestMember_DuplicateCode(t *testing.T){
-//	m1 := &model.Member{Name: "satit", Code: "0816398388"}
-//	fmt.Println(m1)
-//
-//	//act
-//	m1.Reset()
-//
-//
-//	//fist insert code 0816398388
-//	id,err := m1.Create()
-//	fmt.Println("new id is : " , id)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	//fist insert code 0816398388
-//	m1.Name = "999"
-//	id,err = m1.Create()
-//	fmt.Println("new id is : " , id)
-//	fmt.Println(m1)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//
-//}
+func TestMember_DuplicateName(t *testing.T){
+	m1 := &model.Member{Name: "satit", Code: "0816398388"}
+	fmt.Println(m1)
+
+	//act
+	m1.Reset()
+
+
+	//fist insert code 0816398388
+	id,err := m1.Create()
+	fmt.Println("new id is : " , id)
+	fmt.Println(m1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	//fist insert code 0816398388
+	m1.Code = "999"
+	id,err = m1.Create()
+	fmt.Println("new id is : " , id)
+	fmt.Println(m1)
+	if err!=nil {
+		t.Error(err)
+	}
+
+
+}
+
+
+func TestMember_DuplicateCode(t *testing.T){
+	m1 := &model.Member{Name: "satit", Code: "0816398388"}
+	fmt.Println(m1)
+
+	//act
+	m1.Reset()
+
+
+	//fist insert code 0816398388
+	id,err := m1.Create()
+	fmt.Println("new id is : " , id)
+	fmt.Println(m1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	//fist insert code 0816398388
+	m1.Name = "999"
+	id,err = m1.Create()
+	fmt.Println("new id is : " , id)
+	fmt.Println(m1)
+	if err != nil {
+		t.Error(err)
+	}
+
+
+}
--- model/sale_test.go
+++ model/sale_test.go
@@ -8,17 +8,23 @@ import (
 	//"strconv"
 )
 
-func TestSale_GetbyMember(t *testing.T) {
+func TestConnectMsSQL(t *testing.T) {
+	_, err := model.ConnectMssql()
+	if err != nil {
+		fmt.Println("MsSql Connect Error : ", err)
+		return
+	}
+	fmt.Println("connected")
+
+}
+func TestSale_GetSalebyMember(t *testing.T) {
 	s := model.Sale{Arcode:"990002543"}
 	res := s.GetbyMember()
 	var sum int
 	sum = 0
-	for k := range res {
-		fmt.Println("docno:", res[k].Docno , "totalamount : ",res[k].Totalamount )
-		sum = sum+ int(res[k].Totalamount)
+	for _,k := range res {
+		fmt.Println("docno:", k.Docno , "totalamount : ",k.Totalamount )
+		sum = sum+ int(k.Totalamount)
 	}
-
 	fmt.Println("sum totalamount is : ",sum)
-
-
 }
